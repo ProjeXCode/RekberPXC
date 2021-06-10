9515b2a86c309fef98e084c031a54314b891f9f6
ProjeXCode <24662855+ProjeXCode@users.noreply.github.com>
2021-06-10 22:33:08 +0700

    Create rekber-tools.js

---
+++ assets/js/rekber-tools.js
@@ -0,0 +1,33 @@
+$('#api_button').click(function () {
+  let id = $('#api_id_input').val();
+  $('#status_message').html("<span class='status-message'>" + "Mengambil Data..." + "</span>");
+  fetch('https://cdn.jsdelivr.net/gh/ProjeXCode/RekberPXC@master/data/' + id + '.json')
+    .then(res => res.json())
+    .then(res => {
+      $('#rekber_ovo').val(res.OVO);
+      $('#rekber_dana').val(res.DANA);
+      $('#rekber_gopay').val(res.GOPAY);
+      $('#rekber_linkaja').val(res.LINK_AJA);
+      $('#rekber_bca').val(res.BCA);
+      $('#rekber_bni').val(res.BNI);
+      $('#rekber_mandiri').val(res.MANDIRI);
+      $('#rekber_whatsapp').val(res.WHATSAPP);
+      $('#rekber_an').val(res.ATAS_NAMA);
+      $('#rekber_fb_name').html(res.FB_NAME);
+      $('#rekber_fb_url').html(res.FB_URL);
+      $('#rekber_fb_links').attr("href",res.FB_LINKS);
+     })
+    .then(() => {
+      $('#status_message').html("<span class='data-success'>" + "Data Sukses Dimuat." + "</span>");
+      $('#rekber_message').html("<span class='validrekber'>" + "VALID." + "</span>");
+      alert("Data Valid : Silahkan Lanjutkan Transaksi anda.");
+    })
+    .catch(e => {
+     $('#status_message').html("<span class='data-failed'>" + "Data Gagal Dimuat." + "</span>");
+     $('#rekber_message').html("<span class='validrekber'>" + "TIDAK VALID." + "</span>");
+     $('.valid').val("None");
+     $('.validtext').html("None");
+     alert("Data Tidak Valid : Tolong Hentikan/Cek Kembali Rekber Transaksi Anda Asli atau Palsu.");
+      console.log(e)
+    });
+})
