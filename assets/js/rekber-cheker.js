59e855f093e922dfb5a5ee9e7d4311f529f86fd2
ProjeXCode <24662855+ProjeXCode@users.noreply.github.com>
2021-06-10 20:50:46 +0700

    Update rekber-cheker.js

--- assets/js/rekber-cheker.js
+++ assets/js/rekber-cheker.js
@@ -4,24 +4,24 @@ $('#api_button').click(function () {
   fetch('https://cdn.jsdelivr.net/gh/ProjeXCode/RekberPXC@master/data/' + id + '.json')
     .then(res => res.json())
     .then(res => {
-      $('#rekber_ovo').val(res.OVO);
-      $('#rekber_dana').val(res.DANA);
-      $('#rekber_gopay').val(res.GOPAY);
-      $('#rekber_linkaja').val(res.LINK_AJA);
-      $('#rekber_bca').val(res.BCA);
-      $('#rekber_bni').val(res.BNI);
-      $('#rekber_mandiri').val(res.MANDIRI);
-      $('#rekber_whatsapp').val(res.WHATSAPP);
-      $('#rekber_an').val(res.ATAS_NAMA);
-      $('#rekber_fb_name').val(res.FB_NAME);
-      $('#rekber_fb_url').val(res.FB_URL);
+      $('#rekber_ovo').html(res.OVO);
+      $('#rekber_dana').html(res.DANA);
+      $('#rekber_gopay').html(res.GOPAY);
+      $('#rekber_linkaja').html(res.LINK_AJA);
+      $('#rekber_bca').html(res.BCA);
+      $('#rekber_bni').html(res.BNI);
+      $('#rekber_mandiri').html(res.MANDIRI);
+      $('#rekber_whatsapp').html(res.WHATSAPP);
+      $('#rekber_an').html(res.ATAS_NAMA);
+      $('#rekber_fb_name').html(res.FB_NAME);
+      $('#rekber_fb_url').html(res.FB_URL);
      })
     .then(() => {
       $('#status_message').html("<span class='data-success'>" + "Data Sukses Dimuat." + "</span>" + "<span class='success-message'>" + "REKBER VALID SILAHKAN LANJUTKAN TRANSAKSI ANDA" + "</span>");
     })
     .catch(e => {
      $('#status_message').html("<span class='data-failed'>" + "Data Gagal Dimuat." + "</span>" + "<span class='failed-message'>" + "REKBER NO VALID SILAHKAN URUNGKAN NIAT ANDA UNTUK BERTRANSAKSI" + "</span>");
-     $('#valid').html("<span class='data-failed'>" + "Data Gagal Dimuat." + "</span>" + "<span class='failed-message'>" + "REKBER NO VALID SILAHKAN URUNGKAN NIAT ANDA UNTUK BERTRANSAKSI" + "</span>");
+     $('#valid').html("None");
       console.log(e)
     });
 })
