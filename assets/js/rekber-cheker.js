9425c3cc222695f6c761b73ee7fd89261c485164
ProjeXCode <24662855+ProjeXCode@users.noreply.github.com>
2021-06-10 11:56:00 +0700

    Update rekber-cheker.js

--- assets/js/rekber-cheker.js
+++ assets/js/rekber-cheker.js
@@ -1,20 +1,26 @@
 <script>
-$(document).ready(function(){
-$("#submit_url").click(function(){
-  var link_input = document.getElementById('link_rekber').value;
-    url = new Object();
-    url.VALETINEDAY = 20;
-    url.GONGXIFATCAI = 30;
-    url.PROMOQIUQIU = 40;
-    url.QIUQIU = 30;
-    console.log(link_input)
-    if (url[link_input]) {
-          document.getElementById('validurl').innerHTML= "<span class='promo-valid'>" + "Coupon applied!" + "</span>" ;
-          $("#status_rekber").html("Silahkan Bertransaksi,Akun Trusted Oleh System");
-    } else {
-     document.getElementById('validurl').innerHTML= "<span class='promo-novalid'>" + "Coupon not applied!" + "</span>" ;
-       $("#status_rekber").html("Jangan Bertransaksi,Akun Tidak dikenali System");
-    }
- });
- });
+$('#api_button').click(function () {
+  let id = $('#api_id_input').val();
+  $('#api_id_input').html("<span class='status-message'>" + "Mengambil Data..." + "</span>");
+  fetch('https://cdn.jsdelivr.net/gh/ProjeXCode/RekberPXC/data/' + id + '.json')
+    .then(res => res.json())
+    .then(res => {
+      $('#rekber_ovo').val(res.OVO);
+      $('#rekber_dana').val(res.DANA);
+      $('#rekber_gopay').val(res.GOPAY);
+      $('#rekber_linkaja').val(res.LINK_AJA);
+      $('#rekber_bca').val(res.BCA);
+      $('#rekber_bni').val(res.BNI);
+      $('#rekber_mandiri').val(res.MANDIRI);
+      $('#rekber_whatsapp').val(res.WHATSAPP);
+      $('#rekber_an').val(res.ATAS_NAMA);
+     })
+    .then(() => {
+      $('#status_message').html("<span class='data-success'>" + "Data Sukses Dimuat." + "</span>" + "<span class='success-message'>" + "REKBER VALID SILAHKAN LANJUTKAN TRANSAKSI ANDA" + "</span>");
+    })
+    .catch(e => {
+     $('#status_message').html("<span class='data-failed'>" + "Data Gagal Dimuat." + "</span>" + "<span class='failed-message'>" + "REKBER NO VALID SILAHKAN URUNGKAN NIAT ANDA UNTUK BERTRANSAKSI" + "</span>");
+      console.log(e)
+    });
+})
 </script>
