b5e35a8c59d5bec085bb84acfff62abac28a7761
ProjeXCode <24662855+ProjeXCode@users.noreply.github.com>
2021-06-10 20:57:31 +0700

    Update rekber-cheker.js

--- assets/js/rekber-cheker.js
+++ assets/js/rekber-cheker.js
@@ -4,17 +4,17 @@ $('#api_button').click(function () {
   fetch('https://cdn.jsdelivr.net/gh/ProjeXCode/RekberPXC@master/data/' + id + '.json')
     .then(res => res.json())
     .then(res => {
-      $('#rekber_ovo').html(res.OVO);
-      $('#rekber_dana').html(res.DANA);
-      $('#rekber_gopay').html(res.GOPAY);
-      $('#rekber_linkaja').html(res.LINK_AJA);
-      $('#rekber_bca').html(res.BCA);
-      $('#rekber_bni').html(res.BNI);
-      $('#rekber_mandiri').html(res.MANDIRI);
-      $('#rekber_whatsapp').html(res.WHATSAPP);
-      $('#rekber_an').html(res.ATAS_NAMA);
-      $('#rekber_fb_name').html(res.FB_NAME);
-      $('#rekber_fb_url').html(res.FB_URL);
+      $('#rekber_ovo').text(res.OVO);
+      $('#rekber_dana').text(res.DANA);
+      $('#rekber_gopay').text(res.GOPAY);
+      $('#rekber_linkaja').text(res.LINK_AJA);
+      $('#rekber_bca').text(res.BCA);
+      $('#rekber_bni').text(res.BNI);
+      $('#rekber_mandiri').text(res.MANDIRI);
+      $('#rekber_whatsapp').text(res.WHATSAPP);
+      $('#rekber_an').text(res.ATAS_NAMA);
+      $('#rekber_fb_name').text(res.FB_NAME);
+      $('#rekber_fb_url').text(res.FB_URL);
      })
     .then(() => {
       $('#status_message').html("<span class='data-success'>" + "Data Sukses Dimuat." + "</span>" + "<span class='success-message'>" + "REKBER VALID SILAHKAN LANJUTKAN TRANSAKSI ANDA" + "</span>");
